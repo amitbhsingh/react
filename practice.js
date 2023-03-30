9bca6056e64ac563094b9b436793ccc36847feb4
amitkumar singh <amitkumarsingh@amitkumars-MacBook-Pro.local>
2023-03-30 13:32:29 -0400

    practice file with some practice

--- practice.js
+++ practice.js
@@ -2,3 +2,19 @@ var ourStr= "This is the first string! "
 ourStr += "This is the second string"
 console.log(ourStr)
 
+var ourarray= [50,60,70];
+var ourdata=ourarray[0];
+console.log(ourdata)
+
+/* modiy arrays with indexes */
+var our0array=["one","two",3]
+ouarray=our0array[2]
+console.log(ouarray)
+/* push */
+var our1array=["stimpson", "J", "cat"]
+our1array.push(["happy","joy"])
+console.log(our1array)
+/* pop */
+var our2array=[1,2,3];
+var removedfromourarray=our2array.pop();
+console.log(our2array)

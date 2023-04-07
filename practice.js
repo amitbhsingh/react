c406e865ccf863ec97d48c7213cd5db65744c48d
amitkumar singh <amitkumarsingh@amitkumars-MacBook-Pro.local>
2023-04-07 18:24:25 -0400

    practice

--- practice.js
+++ practice.js
@@ -68,3 +68,58 @@ function addFive(){
     sum+=5;
 
 }
+
+/* assignment with a returned value */
+var changed=0;
+function change(num){
+    return (num+5)/3;
+}
+changed=change(10);
+var processed= 0;
+function processarg(num){
+    return (num +3)/5;
+}
+/* stand in line */
+function next(arr,item){
+    //code 
+    arr.push(item);
+    return arr.shift(); // removes the first item and returns it
+    
+}
+
+var testarr=[1,2,3,4,5];
+console.log("Before: ", JSON.stringify(testarr));
+console.log(next(testarr,6));
+console.log("After: "+JSON.stringify(testarr));
+
+/* boolean values */
+
+function wcbool(){
+    return false;
+}
+/* use conditional logic with if statements*/
+
+function ourtrueorfales(isTrue){
+    if (isTrue){
+        return "yes, it's true";
+    }
+    return "no, it's false";
+}
+function trueorfalse(wasthattrue){
+    if(wasthattrue){
+        return "no, that was false"
+    }
+    return "yes, that was true"
+}
+console.log(trueorfalse(true));
+
+/* Comparision with the equality operator*/
+
+function testequal(val){
+    if(val===12){
+        return "equal";
+    }
+    return "not equal";
+}
+console.log(testequal('12'));
+// == is not strict equality check but === is strick
